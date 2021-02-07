176e6e97211428134a82764633b5f17c2aaf25e9
Kryukov_MM <Nordic54321@mail.ru>
2021-02-07 21:01:06 +0300

    3. Арифметические операции. [#457281]

---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculate;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte age = 18;
+        System.out.println(age);
+        double lenth = 78.03;
+        System.out.println(lenth);
+        boolean baby = true;
+        System.out.println(baby);
+        char gender = 'M';
+        System.out.println(gender);
+        short size = 32000;
+        System.out.println(size);
+
+    }
+}
---
+++ src/ru/job4j/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package ru.job4j.ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int onePlusTwo = one + two;
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six / two;
+        int sixMinusTwo = five - two;
+        int sixTimeTwo = five * two;
+        System.out.println(sixDivTwo);
+        System.out.println(sixMinusTwo);
+        System.out.println(sixTimeTwo);
+    }
+}
