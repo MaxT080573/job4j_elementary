126c8668cb33d2e734035e84ffe9efcf45e398cd
Kryukov_MM <Nordic54321@mail.ru>
2021-02-21 22:07:08 +0300

    9.1. String [#457278]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        String lyrics = "But I am a newbie";
+        int year = 2021;
+        String separate = " in ";
+        String z = ".";
+        String id = idea + lyrics + separate + year + z;
+        System.out.println(id);
+    }
+}
