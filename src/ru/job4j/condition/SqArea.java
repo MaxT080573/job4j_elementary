b0e0312282d1704f800a99f77f8bc08de1af5646
Kryukov_MM <Nordic54321@mail.ru>
2021-02-26 22:53:58 +0300

    7. Расстояние между точками в системе координат [#457282]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,9 +2,9 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, int k) {
-        int z = p / (2 * (k + 1));
-        double h = Math.pow(z, 2);
-        return h * k;
+        int h = p / (2 * (k + 1));
+        double l = h * k;
+        return l * h;
 
     }
 
