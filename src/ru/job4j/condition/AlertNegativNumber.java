b934dddc27ae438b49e470038cd1381325f2e357
Kryukov_MM <Nordic54321@mail.ru>
2021-03-11 21:25:24 +0300

    4.1. Операторы ветвлений [#457230]

--- src/ru/job4j/condition/AlertNegativNumber.java
+++ src/ru/job4j/condition/AlertNegativNumber.java
@@ -3,10 +3,11 @@ package ru.job4j.condition;
 public class AlertNegativNumber {
     public static void main(String[] args) {
         possibleDiv(-4);
-        possibleDiv(0);
+
     }
 
     public static void possibleDiv(int number) {
+
         if (number < 0) {
             System.out.println("This is negative numbers");
         }
