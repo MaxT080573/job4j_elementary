57129056261164d37cf2fbe8996321e15013bdc6
Kryukov_MM <Nordic54321@mail.ru>
2021-03-14 21:20:40 +0300

    4.3.4. if c return [#457223]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,7 +1,14 @@
 package ru.job4j;
 
 public class Main {
-    public static void main(String [] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) {
+        int in = 500;
+        int expected = 5;
+        int out = Converter.rubleToPound(in);
+        boolean passed = expected == out;
+        System.out.println("500 rubles are 5 pound. Test result : " + passed);
+        System.out.println("270 rubles are 3 euro. Test result : " + passed);
+        System.out.println("150 rubles are 2 dollar. Test result : " + passed);
     }
+
 }
