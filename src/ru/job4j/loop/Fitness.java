19912612a5bb87c5be20ad7327bf54bf2956125e
Kryukov_MM <Nordic54321@mail.ru>
2021-06-17 13:56:05 +0300

    5.3.Построить шахматную доску в псевдографике

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
+            month++;
+        }
+        return month;
+    }
+
+    public static void main(String[] args) {
+        int ivan = 40;
+        int nik = 90;
+        int result = Fitness.calc(ivan, nik);
+        System.out.println(result);
+    }
+}
+
+
+
